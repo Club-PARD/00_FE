f3b2228d81544bc4c245befe3664e1d6ef9df08d
eunwoo814 <petit10082@gmail.com>
2025-12-31 17:20:59 +0900

    login

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+export default function LoginPage() {
+    return (
+      <main>
+        <Header/>
+        <h1>로그인</h1>
+        <button>구글로 로그인</button>
+      </main>
+    );
+  }
+
+
